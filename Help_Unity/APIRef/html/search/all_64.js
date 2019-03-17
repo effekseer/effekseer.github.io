91da891d8e89a0e803a8024080e133264c9080c7
effekseer <effekseer@gmail.com>
2019-03-17 19:04:45 +0900

    Revert "revert many things"
    This reverts commit acbe97da5cbd05dd8a97180a744633cee8b0c757.

--- Help_Unity/APIRef/html/search/all_64.js
+++ Help_Unity/APIRef/html/search/all_64.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['dostartnetworkautomatically',['DoStartNetworkAutomatically',['../class_effekseer_system.html#a8fbc8f8d2b4986ad50d4e335349b0d00',1,'EffekseerSystem']]],
   ['drawinsceneview',['drawInSceneView',['../class_effekseer_system.html#a469cf59d9deeaef6e27b15c5fc5ee524',1,'EffekseerSystem']]]
 ];
--- Help_Unity/APIRef/html/search/functions_73.js
+++ Help_Unity/APIRef/html/search/functions_73.js
@@ -6,7 +6,9 @@ var searchData=
   ['setrotation',['SetRotation',['../struct_effekseer_handle.html#a54be425dc9d059e66fe00799f34e25cf',1,'EffekseerHandle']]],
   ['setscale',['SetScale',['../struct_effekseer_handle.html#a858d40ff7676ec5c2ae062596f52795b',1,'EffekseerHandle']]],
   ['settargetlocation',['SetTargetLocation',['../class_effekseer_emitter.html#a39366319e460cc573d4bfc40803f6787',1,'EffekseerEmitter.SetTargetLocation()'],['../struct_effekseer_handle.html#aa13ed72d63bdcf16ab9683261b92c855',1,'EffekseerHandle.SetTargetLocation()']]],
+  ['startnetwork',['StartNetwork',['../class_effekseer_system.html#ad317e23d8505642327cf15c06dcc2e09',1,'EffekseerSystem']]],
   ['stop',['Stop',['../class_effekseer_emitter.html#acab7d9ae949cd0aa844ae66b2f44ad5d',1,'EffekseerEmitter.Stop()'],['../struct_effekseer_handle.html#add31723e9d75fad60015c48dfe1bf785',1,'EffekseerHandle.Stop()']]],
   ['stopalleffects',['StopAllEffects',['../class_effekseer_system.html#acf3dde4a65c0e99aeacfea71b517b541',1,'EffekseerSystem']]],
+  ['stopnetwork',['StopNetwork',['../class_effekseer_system.html#ad8dd3bc972839f858dc68a078ff307dc',1,'EffekseerSystem']]],
   ['stoproot',['StopRoot',['../class_effekseer_emitter.html#a6bb4daefa3632e823b684ba575043290',1,'EffekseerEmitter.StopRoot()'],['../struct_effekseer_handle.html#ae16184a6f1770d63edf941941b8ad063',1,'EffekseerHandle.StopRoot()']]]
 ];
